cbc74dc88ab519044f4108f0b1876f9fb35d686d
boryspimpek <borysgame@gmail.com>
2026-05-25 19:58:06 +0200

    waroom

--- scenes/projectile/projectile.gd
+++ scenes/projectile/projectile.gd
@@ -2,11 +2,7 @@ extends Area3D
 
 # --- Parametry pocisku (ustawiane przez WeaponSystem) ---
 @export var velocity: Vector3 = Vector3.ZERO
-@export var acceleration: Vector3 = Vector3.ZERO  # Przyspieszenie po wystrzeleniu
 @export var damage: int = 3
-@export var lifetime: float = 0.0  # Czas życia w sekundach (0 = brak limitu)
-# --- Wewnętrzne ---
-var lifetime_timer: float = 0.0
 
 func _ready():
 	# Warstwa 4 = pocisk gracza; maska 2 = wykrywa wrogów (warstwa 2)
@@ -15,15 +11,7 @@ func _ready():
 	$VisibleOnScreenNotifier3D.screen_exited.connect(queue_free)
 
 func _physics_process(delta: float):
-	velocity += acceleration * delta
-
 	position += velocity * delta
-
-	if lifetime > 0.0:
-		lifetime_timer += delta
-		if lifetime_timer >= lifetime:
-			queue_free()
-			return
 
 func _on_area_entered(area: Area3D):
 	if area.is_in_group("enemies"):
