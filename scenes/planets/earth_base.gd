afbab77fd1de8803638d40662bc779bf464659d7
boryspimpek <borysgame@gmail.com>
2026-05-31 09:27:49 +0200

    enemies

---
+++ scenes/planets/earth_base.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+@export var spin_speed: float = 0.5
+@export var speed: float = 0.0
+
+
+func _process(delta: float) -> void:
+	transform = transform.rotated_local(Vector3.UP, spin_speed * delta)
+	global_position.z += speed * delta
