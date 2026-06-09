e67fb25b7f4091b0af5f8197a377c195d7d7c2a0
boryspimpek <borysgame@gmail.com>
2026-06-09 15:25:08 +0200

    effect smoke

--- assets/BinbunVFX_Vol2/shared/script/VFXControllerBB.gd
+++ assets/BinbunVFX_Vol2/shared/script/VFXControllerBB.gd
@@ -60,22 +60,102 @@ var anim : AnimationPlayer:
 signal finished
 signal stopped
 
-func _ready() -> void:
-	if autoplay: play()
+var _vfx_playing: bool = false
 
-func _enter_tree() -> void:
-	if autoplay: play()
+func _ready() -> void:
+	if autoplay:
+		play()
+
+func _process(_delta: float) -> void:
+	if _vfx_playing:
+		_apply_smoke_decay_tracks()
 
 func play() -> void:
+	if anim == null:
+		return
+
+	_vfx_playing = true
 	anim.play("main")
-	anim.seek(0.0)
-	
+	anim.seek(0.0, true)
+	_prepare_smoke_effects()
+	_apply_smoke_decay_tracks()
+
 	await anim.animation_finished
+	_vfx_playing = false
 	finished.emit()
-	
+
 	if !one_shot:
 		anim.advance(0.0)
 		play()
+
+func _is_smoke_material(mat: ShaderMaterial) -> bool:
+	if mat == null or mat.shader == null:
+		return false
+	var path := mat.shader.resource_path
+	return path.ends_with("explosion_smoke.gdshader")
+
+func _prepare_smoke_effects() -> void:
+	for node in get_children():
+		if node is GPUParticles3D:
+			var mat := (node as GPUParticles3D).material_override as ShaderMaterial
+			if _is_smoke_material(mat):
+				(node as GPUParticles3D).restart()
+				(node as GPUParticles3D).emitting = true
+
+func _apply_smoke_decay_tracks() -> void:
+	if anim == null or anim.current_animation.is_empty():
+		return
+
+	var animation := anim.get_animation(anim.current_animation)
+	if animation == null:
+		return
+
+	var time := anim.current_animation_position
+	for track_idx in animation.get_track_count():
+		var path := animation.track_get_path(track_idx)
+		var path_str := str(path)
+		if not path_str.contains("shader_parameter/effect_decay"):
+			continue
+
+		var node_path := NodePath(path_str.get_slice(":", 0))
+		var node := get_node_or_null(node_path)
+		if node == null:
+			continue
+
+		var value := _sample_track_value(animation, track_idx, time)
+		var mat: ShaderMaterial = null
+		if node is MeshInstance3D:
+			mat = (node as MeshInstance3D).material_override as ShaderMaterial
+		elif node is GPUParticles3D:
+			mat = (node as GPUParticles3D).material_override as ShaderMaterial
+
+		if _is_smoke_material(mat):
+			mat.set_shader_parameter("effect_decay", value)
+
+func _sample_track_value(animation: Animation, track_idx: int, time: float) -> float:
+	var key_count := animation.track_get_key_count(track_idx)
+	if key_count == 0:
+		return 0.0
+
+	var times := PackedFloat32Array()
+	var values := PackedFloat32Array()
+	for key_idx in key_count:
+		times.append(animation.track_get_key_time(track_idx, key_idx))
+		values.append(animation.track_get_key_value(track_idx, key_idx))
+
+	if time <= times[0]:
+		return values[0]
+	if time >= times[key_count - 1]:
+		return values[key_count - 1]
+
+	for key_idx in key_count - 1:
+		var t0 := times[key_idx]
+		var t1 := times[key_idx + 1]
+		if time >= t0 and time <= t1:
+			var blend := (time - t0) / maxf(t1 - t0, 0.00001)
+			return lerpf(values[key_idx], values[key_idx + 1], blend)
+
+	return values[key_count - 1]
 
 func stop() -> void:
 	anim.play("stop")
