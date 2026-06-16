4fcbead002798489956f592e252b4944d3ddaeb8
boryspimpek <borysgame@gmail.com>
2026-06-16 17:19:28 +0200

    delete planets

--- scenes/planets/earth_base.gd
+++ scenes/planets/earth_base.gd
@@ -1,9 +1,25 @@
 extends Node3D
 
-@export var spin_speed: float = 0.1
+## Obrót planety: AnimationPlayer (spin). Ten skrypt tylko przesuwa planetę wzdłuż Z.
+@export var spin_speed: float = 0.1:
+	set(value):
+		spin_speed = value
+		_apply_spin_speed()
 @export var speed: float = 1.0
+
+const BASE_SPIN_SPEED := 0.1
+
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	_apply_spin_speed()
+
+
+func _apply_spin_speed() -> void:
+	if _animation_player:
+		_animation_player.speed_scale = spin_speed / BASE_SPIN_SPEED
 
 
 func _process(delta: float) -> void:
-	rotate_object_local(Vector3.UP, spin_speed * delta)
 	global_position.z += speed * delta
--- scenes/world/negative_sun.gd
+++
@@ -1,5 +0,0 @@
-extends DirectionalLight3D
-
-func _ready():
-	# Kod pozwala ominąć blokadę edytora i wymusza ujemną energię
-	light_energy = -1.0
