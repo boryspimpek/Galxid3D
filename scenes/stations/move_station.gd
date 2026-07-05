d90c327e77916a0cccc9d120334c17e92302441a
boryspimpek <borysgame@gmail.com>
2026-07-05 18:07:12 +0200

    stations

--- scenes/stations/move_station.gd
+++ scenes/stations/move_station.gd
@@ -1,6 +1,11 @@
 extends Node3D
 
-@export var speed: float = 5
+@export var speed_z: float = 2
+@export var speed_x: float = 0
+@export var speed_y: float = 0
 
 func _process(delta: float) -> void:
-	position.z += speed * delta
+	position.z += speed_z * delta
+	position.x += speed_x * delta
+	rotation.y += speed_y * delta
+	
