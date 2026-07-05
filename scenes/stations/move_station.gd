9fd1120c4c2809179f75629f6b2d9bb12510fa2e
boryspimpek <borysgame@gmail.com>
2026-07-05 19:37:41 +0200

    stations

--- scenes/stations/move_station.gd
+++ scenes/stations/move_station.gd
@@ -7,18 +7,27 @@ extends Node3D
 @export_group("Activation")
 @export var activate_on_scroll_line: bool = false
 
+@export_group("Cleanup")
+@export var despawn_off_screen: bool = true
+@export var despawn_margin: float = 30.0
+
 var _scroll_activation_z: float = -12.0
 var _active: bool = false
+var _despawn_z: float = 12.0
+var _despawn_with_notifier: bool = false
 
 
 func _ready() -> void:
 	var play_area := DataManager.get_play_area_config()
 	if play_area:
 		_scroll_activation_z = play_area.scroll_activation_z
+		_despawn_z = play_area.frame_half_z + despawn_margin
 
 	if not activate_on_scroll_line:
 		_active = true
 
+	if despawn_off_screen:
+		_despawn_with_notifier = _setup_despawn_notifier()
 
 func _process(delta: float) -> void:
 	if activate_on_scroll_line and not _active:
@@ -30,3 +39,21 @@ func _process(delta: float) -> void:
 	position.z += speed_z * delta
 	position.x += speed_x * delta
 	rotation.y += speed_y * delta
+
+	if despawn_off_screen and not _despawn_with_notifier and global_position.z > _despawn_z:
+		queue_free()
+
+
+func _setup_despawn_notifier() -> bool:
+	var notifier := get_node_or_null("VisibleOnScreenNotifier3D") as VisibleOnScreenNotifier3D
+	if notifier == null:
+		return false
+
+	notifier.visible = true
+	notifier.screen_exited.connect(_on_screen_exited)
+	return true
+
+
+func _on_screen_exited() -> void:
+	if despawn_off_screen:
+		queue_free()
